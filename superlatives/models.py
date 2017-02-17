9c9af34f07de05fff5abb7b9b0a02c4994f780cb
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-02-16 23:02:38 -0500

    Upgrade migration to use JSON blob for answers

--- superlatives/models.py
+++ superlatives/models.py
@@ -1,5 +1,5 @@
 # CSH Superlatives Models
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Text
 from superlatives import db
 
 class Person(db.Model):
@@ -20,43 +20,7 @@ class SuperlativeVote(db.Model):
     __tablename__ = 'superlatives'
     id = Column(Integer, primary_key=True)
     # 0 and 1 are for the couple entry
-    superlative_0 = Column(Integer, nullable=False)
-    superlative_1 = Column(Integer, nullable=False)
-    superlative_2 = Column(Integer, nullable=False)
-    superlative_3 = Column(Integer, nullable=False)
-    superlative_4 = Column(Integer, nullable=False)
-    superlative_5 = Column(Integer, nullable=False)
-    superlative_6 = Column(Integer, nullable=False)
-    superlative_7 = Column(Integer, nullable=False)
-    superlative_8 = Column(Integer, nullable=False)
-    superlative_9 = Column(Integer, nullable=False)
-    superlative_10 = Column(Integer, nullable=False)
-    superlative_11 = Column(Integer, nullable=False)
-    superlative_12 = Column(Integer, nullable=False)
-    superlative_13 = Column(Integer, nullable=False)
-    superlative_14 = Column(Integer, nullable=False)
-    superlative_15 = Column(Integer, nullable=False)
-    superlative_16 = Column(Integer, nullable=False)
-    superlative_17 = Column(Integer, nullable=False)
-    superlative_18 = Column(Integer, nullable=False)
-    superlative_19 = Column(Integer, nullable=False)
-    superlative_20 = Column(Integer, nullable=False)
-    superlative_21 = Column(Integer, nullable=False)
-    superlative_22 = Column(Integer, nullable=False)
-    superlative_23 = Column(Integer, nullable=False)
-    superlative_24 = Column(Integer, nullable=False)
-    superlative_25 = Column(Integer, nullable=False)
-    superlative_26 = Column(Integer, nullable=False)
-    superlative_27 = Column(Integer, nullable=False)
-
-    def __setitem__(self, item, value):
-        self.__dict__[item] = value
-
-    def __getitem__(self, item):
-        return self.__dict__[item]
+    data = Column(Text, nullable=False)
 
     def __init__(self, answers):
-        i = 0
-        for answer in answers:
-            self.__setitem__("superlative_" + str(i), answer)
-            i += 1
+        self.data = json.dumps(answers)
